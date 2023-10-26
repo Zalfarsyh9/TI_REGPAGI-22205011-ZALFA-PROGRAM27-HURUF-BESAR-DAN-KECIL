f29f018a18cb496269066560b314915769d758d0
Zalfarsyh9 <145966652+Zalfarsyh9@users.noreply.github.com>
2023-10-26 13:09:16 +0700

    Add files via upload

---
+++ src/main/java/com/mycompany/ti_regpagi22205011/latihan27/zalfa/java/TI_REGPAGI22205011Latihan27ZalfaJava.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ti_regpagi22205011.latihan27.zalfa.java;
+import java.util.Scanner;
+/**
+ *
+ * @author User
+ */
+public class TI_REGPAGI22205011Latihan27ZalfaJava {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        boolean ulangi = true;
+        
+        while (ulangi) {
+            System.out.println("Masukan kalimat : ");
+            String kalimat = input.next();
+            
+            String kalimatKecil = kalimat.toLowerCase();
+            String kalimatBesar = kalimat.toUpperCase();
+            
+            System.out.println("==== Hasil formating ===="); 
+            System.out.println("HurufBesar = " +kalimatBesar);
+            System.out.println("HurufKecil = " +kalimatKecil);
+            
+            System.out.println("\nUlangi program? (ya/tidak)");
+            String ulang = input.next();
+            if(ulang == "ya"){
+                System.out.println("Program akan diulangi");
+            }else if(ulang == "tidak"){
+                System.out.println("Program berhenti terimakasih");
+                ulangi = false;
+            }
+        }
+    }
+}
